87c50fe26529329ddb5bd3784b2a01cc83e279e8
Caio Melo <caiofelipee17@gmail.com>
2020-05-28 23:15:04 -0300

    Add Go-Barber Node Backend

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'bb74bc9dba0b6313e542de1510a4b188',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  const authentiateUser = new AuthenticateUserService();
+
+  const { user, token } = await authentiateUser.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return res.json({ user, token });
+});
+
+export default sessionsRouter;
