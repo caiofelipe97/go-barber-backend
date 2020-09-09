74993fba3187b5c6e16e3fa8f17142cf9d8d91ce
Caio Melo <caiofelipee17@gmail.com>
2020-09-08 23:38:01 -0300

    Using Query Params

--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -6,16 +6,16 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
 
-    const listProviderMOnthAvailability = container.resolve(
+    const listProviderMonthAvailability = container.resolve(
       ListProviderMonthAvailabilityService,
     );
 
-    const monthAvailability = await listProviderMOnthAvailability.execute({
+    const monthAvailability = await listProviderMonthAvailability.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(monthAvailability);
--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -42,12 +42,12 @@ class ListProviderMonthAvailability {
     );
 
     const currentDate = new Date(Date.now());
-
     const availability = eachDayArray.map((day) => {
       const appointmentsInDay = appointments.filter((appointment) => {
         return getDate(appointment.date) === day;
       });
       const compareDate = new Date(year, month - 1, day + 1);
+
       return {
         day,
         available:
